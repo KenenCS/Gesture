f6b3e1366b9361934bb84d3b76fb796208c8f60c
KenenCS <KenenCS@163.com>
2016-06-01 10:00:23 +0800

    Revert "Kenen"
    This reverts commit 3b094ebedeae13081ba94f60da6a765ea4e35d3a.

--- IT0506UI手势/IT0506UI手势/AppDelegate.m
+++ IT0506UI手势/IT0506UI手势/AppDelegate.m
@@ -17,8 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
-    NSLog(@"add NSLong");
     return YES;
 }
 
